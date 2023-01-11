0615994511520c591aebe297b132ce33fdc88149
longjl1 <christinakevin158@gmail.com>
2023-01-11 19:57:51 +0800

    Add local mock data for current work, tasks, and history

---
+++ lib/data.ts
@@ -0,0 +1,48 @@
+export const currentTask = {
+  title: "Draft the weekly review memo",
+  block: "Writing block",
+  detail: "Isolate one clean 25-minute pass and leave the polish for later.",
+};
+
+export const completedTasks = [
+  "Inbox cleared and tagged",
+  "Slides trimmed to a final outline",
+  "Research notes grouped by theme",
+];
+
+export const todoItems = [
+  "Rewrite the opening paragraph",
+  "Book a 10-minute recovery break",
+  "Archive yesterday's loose notes",
+  "Queue the afternoon planning pass",
+];
+
+export const sessionStrip = [
+  { label: "Warm-up", minutes: 10 },
+  { label: "Focus", minutes: 25 },
+  { label: "Break", minutes: 5 },
+  { label: "Focus", minutes: 25 },
+  { label: "Review", minutes: 15 },
+];
+
+export const recentHistory = [
+  { day: "Mon", date: "Dec 18", total: "2h 10m", done: 4, carry: 1 },
+  { day: "Tue", date: "Dec 19", total: "3h 05m", done: 5, carry: 0 },
+  { day: "Wed", date: "Dec 20", total: "1h 45m", done: 3, carry: 2 },
+  { day: "Thu", date: "Dec 21", total: "2h 40m", done: 4, carry: 1 },
+  { day: "Fri", date: "Dec 22", total: "2h 55m", done: 5, carry: 1 },
+];
+
+export const monthGrid = [
+  0, 2, 3, 1, 0, 4, 2,
+  1, 3, 2, 0, 4, 3, 1,
+  2, 1, 0, 3, 4, 2, 2,
+  0, 1, 3, 2, 4, 1, 0,
+  3, 4, 2, 1, 0, 2, 4,
+];
+
+export const archivedTracks = [
+  "Research sprint archive",
+  "Admin cleanup archive",
+  "Interview prep archive",
+];
